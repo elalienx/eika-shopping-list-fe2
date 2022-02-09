e80f0d27950cda71b5a0fb685c0e1808139a9052
Eduardo <el_alienx@hotmail.com>
2022-02-09 10:28:08 +0100

    finish the toggle by using a ternary operator connected to the list length

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+// NPM packages
+import { useState } from "react";
+
+// Project files
+import WelcomeScreen from "./screens/WelcomeScreen";
+import ShoppingScreen from "./screens/ShoppingScreen";
+
 export default function App() {
+  // Local state
+  const [list, setList] = useState([]);
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      {/* if list.length is empty show Welcome else show ShoppingScreen */}
+      {/* is is list.length equal 0? */}
+      {/* Ternary operation ?: */}
+      {list.length === 0 ? <WelcomeScreen /> : <ShoppingScreen />}
     </div>
   );
 }
---
+++ src/screens/ShoppingScreen.jsx
@@ -0,0 +1,7 @@
+export default function ShoppingScreen() {
+  return (
+    <div>
+      <h1>Shopping list</h1>
+    </div>
+  );
+}
